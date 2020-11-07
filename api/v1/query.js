59e81497b5bb29c19805c66618dd66ea21f3077b
Rafael Castelo <rbolso@gmail.com>
2020-11-07 02:55:08 -0300

    Enhance route params

--- api/v1/query.js
+++ api/v1/query.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { searchPartieTerm } = require('../../services/query')
 
-router.get('/:politicalParty/:topic', async (req, res, next) => {
+router.get('/:politicalParty&:topic', async (req, res, next) => {
 	const { politicalParty, topic } = req.params
     const relevantData = await searchPartieTerm(politicalParty, topic)
     res.status(200).json(relevantData);

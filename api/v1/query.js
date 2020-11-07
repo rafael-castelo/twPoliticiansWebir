b371fe49745c260749c6ea57d12f172ef599dc14
Rafael Castelo <rbolso@gmail.com>
2020-11-07 00:12:15 -0300

    Minor fixes

--- api/v1/query.js
+++ api/v1/query.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const { searchPartieTerm } = require('../../services/query')
 
-router.get('/:partie/:term', async (req, res, next) => {
-	const {partie, term} = req.params
-    const relevantData = await searchPartieTerm(partie, term)
+router.get('/:politicalParty/:topic', async (req, res, next) => {
+	const { politicalParty, topic } = req.params
+    const relevantData = await searchPartieTerm(politicalParty, topic)
     res.status(200).json(relevantData);
 });
 

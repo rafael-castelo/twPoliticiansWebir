9229039a8a60077147c1d14e86f91d9819ecc19c
Diego <diego@mail.com>
2020-11-06 21:05:35 -0300

    Fin.

--- app.js
+++ app.js
@@ -3,14 +3,13 @@ const app = express();
 const port = 3000;
 
 const { loadDB } = require("./services/loadDatabase.js");
-const { search } = require("./helpers/database.js");
+const { relevantUser } = require("./services/relevantUser.js");
 const test = async () => {
   await loadDB("Partido Nacional");
   await loadDB("Partido Colorado");
   await loadDB("Frente Amplio");
   await loadDB("Cabildo Abierto");
-  const res = await search("frente_amplio", "Lacalle Pou");
-  console.log(res);
+  const user = await relevantUser("frente_amplio", "Lacalle Pou");
 };
 test();
 
